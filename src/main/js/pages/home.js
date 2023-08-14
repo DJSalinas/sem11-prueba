5011d905cd981ca0ff75705cdf5c596e666cfd58
DJSalinas <123711741+DJSalinas@users.noreply.github.com>
2023-08-14 17:19:49 +0000

    nuevo-alumno

---
+++ src/main/js/pages/home.js
@@ -0,0 +1,107 @@
+const React = require('react');
+const client = require('../client');
+const {Link} = require('react-router-dom')
+
+class HomePage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {alumnos: [], cursos: []};
+	}
+	componentDidMount() {
+		client({method: 'GET', path: '/api/alumnos'}).done(response => {
+			this.setState({alumnos: response.entity._embedded.alumnos});
+		});
+
+		client({method: 'GET', path: '/api/cursos'}).done(response => {
+			this.setState({cursos: response.entity._embedded.cursos});
+		});
+	}
+	render() {
+		return (
+			<>
+				<h1>Hola!! Mi Aplicativo</h1>
+				<Titulo entidad="Alumnos" emoji="👨‍🎓" />
+				<AlumnoList alumnos={this.state.alumnos}/>
+                <Link to="/nuevo-alumno">Nuevo Alumno</Link>
+				<Titulo entidad="Cursos" emoji="📝" />
+				<CursoList cursos={this.state.cursos}/>
+			</>
+		)
+	}
+}
+
+//Funcion de flecha
+const Titulo = (props) => {
+	return (
+		<>
+			<hr />
+			<h2>{props.emoji} - {props.entidad}</h2>
+			<hr />
+			Lista completa de {props.entidad.toLowerCase()}
+		</>
+	)
+}
+
+class AlumnoList extends React.Component{
+	render() {
+		const alumnos = this.props.alumnos.map(alumno =>
+			<Alumno key={alumno._links.self.href} alumno={alumno}/>
+		);
+		return (
+			<table border="1">
+				<tbody>
+					<tr>
+						<th>Nombre</th>
+						<th>Apellido</th>
+						<th>Código</th>
+					</tr>
+					{alumnos}
+				</tbody>
+			</table>
+		)
+	}
+}
+
+class CursoList extends React.Component{
+	render() {
+		const cursos = this.props.cursos.map(curso =>
+			<Curso key={curso._links.self.href} curso={curso}/>
+		);
+		return (
+			<table border="1">
+				<tbody>
+					<tr>
+						<th>Nombre</th>
+						<th>Creditos</th>
+					</tr>
+					{cursos}
+				</tbody>
+			</table>
+		)
+	}
+}
+
+class Alumno extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.alumno.nombre}</td>
+				<td>{this.props.alumno.apellido}</td>
+				<td>{this.props.alumno.codigo}</td>
+			</tr>
+		)
+	}
+}
+
+class Curso extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.curso.nombre}</td>
+				<td>{this.props.curso.creditos}</td>
+			</tr>
+		)
+	}
+}
+
+module.exports = HomePage
---
+++ src/main/js/pages/nuevo-alumno.js
@@ -0,0 +1,42 @@
+const React = require('react');
+const {useState} = require('react')
+const { Link, Form } = require('react-router-dom');
+const client = require('../client');
+
+
+const NuevoAlumnoPage = () => {
+
+    const [nombre, setNombre] = useState('')
+    const [apellido, setApellido] = useState('')
+    const [codigo, setCodigo] = useState('')
+
+    const handleSubmit = (evento) => {
+        evento.preventDefault();
+        client({
+            method: 'POST',
+            path:'/api/alumnos',
+            entity:{nombre:nombre, apellido:apellido, codigo:codigo},
+            headers: {'Content-Type': 'application/json'}
+        }).done(() => {
+            window.location = '/';
+        })
+    }
+
+    return (
+        <>
+            <h1>Nuevo Alumno</h1>
+            <Form onSubmit={handleSubmit}>
+                <label>Nombre</label><br/>
+                <input type='text' id='nombre' name='nombre' onChange={e=>setNombre(e.target.value)}/><br/>
+                <label>Apellido</label><br/>
+                <input type='text' id='apellido' name='apellido' onChange={e=>setApellido(e.target.value)}/><br/>
+                <label>Código</label><br/>
+                <input type='text' id='codigo' name='codigo' onChange={e=>setCodigo(e.target.value)}/><br/>
+                <input type='submit' value="Guardar Alumno" />
+            </Form>
+            <Link to="/">Volver</Link>
+        </>
+    )
+}
+
+module.exports = NuevoAlumnoPage;
